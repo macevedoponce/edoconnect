fa2b7c34570ef4b990b71fb67fccbd60765728b5
Miguel Acevedo <82989924+macevedoponce@users.noreply.github.com>
2023-05-23 18:32:25 -0500

    comienzo del proyecto

---
+++ app/src/main/java/com/acevedo/educonnect/Splash/SplashActivity.java
@@ -0,0 +1,53 @@
+package com.acevedo.educonnect.Splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.acevedo.educonnect.Login.LoginActivity;
+import com.acevedo.educonnect.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    TextView txtDerechos, txtOne, txtAño;
+    ImageView imgLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //animacion
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.des_top);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.des_bot);
+
+        txtDerechos = findViewById(R.id.txtDerechos);
+        txtAño = findViewById(R.id.txtAño);
+        txtOne = findViewById(R.id.txtOne);
+        imgLogo = findViewById(R.id.imgLogo);
+
+        txtDerechos.setAnimation(animacion1);
+        txtAño.setAnimation(animacion2);
+        txtOne.setAnimation(animacion2);
+        imgLogo.setAnimation(animacion2);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                i.putExtra("tipo_user",1);
+                startActivity(i);
+                finish();
+            }
+        },3600);
+    }
+}
---
+++ app/src/main/java/com/acevedo/educonnect/Util/Util.java
@@ -0,0 +1,6 @@
+package com.acevedo.educonnect.Util;
+
+public class Util {
+    public static final String RUTA="http://192.168.1.46/educonnectAPI"; //local
+    public static final String RUTA_LOGIN=RUTA+"/validar_usuario.php";
+}
