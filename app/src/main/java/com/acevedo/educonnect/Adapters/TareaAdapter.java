e8f13791f0a3b1c1c0d3781fab86c50637a2fa6f
Miguel Acevedo <82989924+macevedoponce@users.noreply.github.com>
2023-05-27 16:00:22 -0500

    actualizar estado tarea 100%

--- app/src/main/java/com/acevedo/educonnect/Adapters/TareaAdapter.java
+++ app/src/main/java/com/acevedo/educonnect/Adapters/TareaAdapter.java
@@ -1,12 +1,14 @@
 package com.acevedo.educonnect.Adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
@@ -15,9 +17,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.acevedo.educonnect.Clases.Curso;
 import com.acevedo.educonnect.Clases.Tarea;
 import com.acevedo.educonnect.R;
+import com.acevedo.educonnect.Util.Util;
+import com.acevedo.educonnect.ui.docente.curso.tarea.CrearTareaActivity;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TareaAdapter extends RecyclerView.Adapter<TareaAdapter.TareaHolder> implements View.OnClickListener {
 
@@ -50,10 +62,24 @@ public class TareaAdapter extends RecyclerView.Adapter<TareaAdapter.TareaHolder>
     public void onBindViewHolder(@NonNull TareaAdapter.TareaHolder holder, int position) {
         Tarea tarea = tareaList.get(position);
         String titulo = tarea.getTitulo();
+        int id_tarea = tarea.getId();
         int estado = tarea.getEstado();
 
         holder.setTitulo(titulo);
         holder.setEstado(estado);
+        holder.tbEstadoTarea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int estado_boton;
+                if(holder.tbEstadoTarea.isChecked()){
+                    estado_boton = 1;
+                }else{
+                    estado_boton = 2;
+                }
+
+                holder.actualizarEstado(id_tarea, estado_boton);
+            }
+        });
     }
 
     @Override
@@ -72,13 +98,15 @@ public class TareaAdapter extends RecyclerView.Adapter<TareaAdapter.TareaHolder>
         //RadioButton rbEstadoTarea;
         ToggleButton tbEstadoTarea;
         View view;
+
+        RequestQueue requestQueue;
         public TareaHolder(@NonNull View itemView) {
             super(itemView);
             view = itemView;
             tvTituloTarea = view.findViewById(R.id.tvTituloTarea);
             //rbEstadoTarea = view.findViewById(R.id.rbEstadoTarea);
             tbEstadoTarea = view.findViewById(R.id.tbEstadoTarea);
-
+            requestQueue = Volley.newRequestQueue(context);
         }
 
         public void setTitulo(String titulo) {
@@ -92,6 +120,36 @@ public class TareaAdapter extends RecyclerView.Adapter<TareaAdapter.TareaHolder>
             }else{
                 tbEstadoTarea.setChecked(false);
             }
+        }
+
+        public void actualizarEstado(int id_tarea, int estado_boton) {
+            String url = Util.RUTA_ACTUALIZAR_TAREA;
+            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+//                    dialogRegistroExitoso(titulo,descripcion,fechaSeleccionada,response);
+                    if(estado_boton == 1){
+                        Toast.makeText(context, "Tarea Activada", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(context, "Tarea Desactivada ", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(context, "error " + error, Toast.LENGTH_SHORT).show();
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("estado_id", String.valueOf(estado_boton));
+                    params.put("id_tarea", String.valueOf(id_tarea));
+                    return params;
+                }
+            };
+            requestQueue.add(request);
         }
     }
 }
